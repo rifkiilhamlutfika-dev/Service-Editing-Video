c62e5feb421a61b468ccf5cc185f8a62ead7464f
rifkiilhamlutfika-dev <rifkiilhamlutfika.dev@gmail.com>
2025-06-08 16:42:09 +0700

    debug App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Instagram, Mail, MessageCircle, Phone } from "lucide-react";
-import Section1 from "./sections/section1";
+import Section1 from "./sections/Section1";
 import Section2 from "./sections/Section2";
 import Section3 from "./sections/Section3";
 import Section4 from "./sections/Section4";
